48cbe6baa82667d3f2547153c8610c553359e5db
runbang.hu <rhu11@stevens.edu>
2021-11-09 20:00:24 -0500

    basic version

--- matrixmul.v
+++ matrixmul.v
@@ -1,40 +1,76 @@
-module Calculator(A,B,Result);
-
-	input [143:0] A;
-	input [143:0] B;
-	output [143:0] Result;
-	
-	
-	reg [143:0] Result;
-	reg [15:0] A1 [0:2][0:2];
-	reg [15:0] B1 [0:2][0:2];
-	reg [15:0] Res1 [0:2][0:2];
-	
+module Calculator(
+	input Clock,
+	input reset,
+	input [143:0] A, 
+	input [143:0] B,
+	output reg[143:0] Result,
+	output reg done
+);
+	reg [15:0] A1 [2:0][2:0];
+	reg [15:0] B1 [2:0][2:0];
+	reg [15:0] Res1 [2:0][2:0];
+	reg finish;
+	reg init;
 	integer i,j,k;
 	
-	always@ (A or B)
+	always@ (posedge reset)
 	begin
-		//We convert the 1D arrays into 2D
-		{A1[0][0],A1[0][1],A1[0][2],A1[1][0],A1[1][1],A1[1][2],A1[2][0],A1[2][1],A1[2][2]} = A;
-		{B1[0][0],B1[0][1],B1[0][2],B1[1][0],B1[1][1],B1[1][2],B1[2][0],B1[2][1],B1[2][2]} = B;
-		{Res1[0][0],Res1[0][1],Res1[0][2],Res1[1][0],Res1[1][1],Res1[1][2],Res1[2][0],Res1[2][1],Res1[2][2]} = 144'd0;
-		
-		i=0; j=0; k=0;
-		
-		//$display ("Multiplying");
-		
-		for(i=0;i<3;i=i+1)
-		begin
-			for(j=0;j<3;j=j+1)
-			begin
-				for(k=0;k<3;k=k+1)
-				begin
-					Res1[i][j]=Res1[i][j]+ (A1[i][k]*B1[k][j]);
-				end
+		if(reset == 1)begin
+			i = 0;
+			j = 0;
+			k = 0;
+			init = 1;
+			finish = 0;
+			for(i=0;i<=2;i=i+1) begin
+				for(j=0;j<=2;j=j+1) begin
+					A1[i][j] = 16'd0;
+					B1[i][j] = 16'd0;
+					Res1[i][j] = 16'd0;
+            	end 
+        	end 
+		end
+
+		else if(finish ==0)begin
+			if(init ==1)begin
+				for(i=0;i<=2;i=i+1) begin
+                    for(j=0;j<=2;j=j+1) begin
+                        A1[i][j] = A[(i*3+j)*16 +:16];
+                        B1[i][j] = B[(i*3+j)*16 +:16];
+                        Res1[i][j] = 16'd0;
+                    end 
+                end
+				init = 0;
+				i=0; 
+				j=0; 
+				k=0;
+			end
+			else if(finish == 0) begin
+				Res1[i][j] = Res1[i][j] + A1[i][k]*B1[k][j];
+				if(k == 2) begin
+                    k = 0;
+                    if(j == 2) begin
+                        j = 0;
+                        if (i == 2) begin
+                            i = 0;
+                            finish = 1;
+                        end
+                        else
+                            i = i + 1;
+                    end
+                    else
+                        j = j+1;    
+                end
+                else
+                    k = k+1;
+			end
+			else if(finish ==1)begin
+				for(i=0;i<=2;i=i+1) begin   
+                    for(j=0;j<=2;j=j+1) begin 
+                        Result[(i*3+j)*16 +:16] = Res1[i][j];
+                    end
+                end
 			end
 		end
-		
-		Result = {Res1[0][0],Res1[0][1],Res1[0][2],Res1[1][0],Res1[1][1],Res1[1][2],Res1[2][0],Res1[2][1],Res1[2][2]};
 	end
 
 endmodule
--- test_bench.v
+++ test_bench.v
@@ -1,28 +1,44 @@
 `timescale 1ns / 1ns
-module test_bench();
+module test_bench;
 	
 	reg [143:0] A;
 	reg [143:0] B;
-	
 	wire [143:0] Answer;
-	
-	Calculator calculator(.A(A), .B(B), .Result(Answer));
-	
-	
-    initial begin
-        
+	reg reset;
+	reg Clock;
+	reg[15:0] matC[2:0][2:0];
+	integer i,j;
+	parameter Clock_period = 2;
+	initial
+	begin
+		Clock = 1;
+		reset = 1;
+		#5;   
+		reset = 0; 
+		#Clock_period;
+		A = {8'd9,8'd8,8'd7,8'd6,8'd5,8'd4,8'd3,8'd2,8'd1};
+		B = {8'd1,8'd9,8'd8,8'd7,8'd6,8'd5,8'd4,8'd3,8'd2};
+		#(Clock_period/2); 
+		for(i=0;i<=2;i=i+1) begin
+			for(j=0;j<=2;j=j+1) begin
+				matC[i][j] = Answer[(i*3+j)*8+:8];
+			end
+		end
+		#5;
+		$stop;  
+	end
+    initial begin 
         $dumpfile("wave.vcd");        
         $dumpvars(0, test_bench);
     end
-    
-	initial begin
-		
-		//Initial inputs
-		A={16'd1,16'd2,16'd3,16'd4,16'd5,16'd6,16'd7,16'd8,16'd9};
-		B={16'd7,16'd3,16'd5,16'd12,16'd11,16'd17,16'd20,16'd3,16'd0};
-		
-	end
+	always #(Clock_period/2) Clock <= ~Clock;
 
-
+	Calculator calculator 
+        (.Clock(Clock), 
+        .reset(reset), 
+        .A(A),
+        .B(B), 
+        .Result(Answer),
+        .done(done));
 
 endmodule
